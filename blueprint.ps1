c95301c8d851b90ef9be5c593926c4df6290406d
Edouard Topin <ed.topin@gmail.com>
2019-07-12 17:23:59 +0200

    split files

---
+++ blueprint.ps1
@@ -0,0 +1,23 @@
+#!/usr/bin/pwsh
+param(
+[string]$sourceVra,$tenant, $username, $password
+)
+# Check if Module Exist
+Import-Module -Name PowervRA
+
+
+# setup package folder
+$path = "./package"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+# Init credantials
+$secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
+
+#connect to the source vRA instance
+Connect-vRAServer -Server $sourceVra -Tenant $tenant -Credential $cred  -IgnoreCertRequirements
+
+Get-vRABlueprint | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File $path/blueprints.json
