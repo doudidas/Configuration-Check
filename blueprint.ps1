48e67f02d54005deaabf76f7200aafb753313236
Edouard Topin <ed.topin@gmail.com>
2019-07-12 17:39:31 +0200

    test

--- blueprint.ps1
+++ blueprint.ps1
@@ -1,21 +1,13 @@
-#!/usr/bin/pwsh
-param(
-[string]$sourceVra,$tenant, $username, $password
-)
-# Check if Module Exist
-Import-Module -Name PowervRA
-
-
-# setup package folder
-$path = "./package"
-If(!(test-path $path))
-{
-      New-Item -ItemType Directory -Force -Path $path
-}
+$s = GET-Content ./session.json | ConvertFrom-Json
+ $sourceVra = $s.url
+ $tenant = $s.tenant
+ $username = $s.username
+ $password = $s.password
 
 # Init credantials
 $secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
+
 
 #connect to the source vRA instance
 Connect-vRAServer -Server $sourceVra -Tenant $tenant -Credential $cred  -IgnoreCertRequirements
--- init.ps1
+++ init.ps1
@@ -1,9 +1,5 @@
-param(
-[string]$sourceVra,$tenant, $username, $password
-)
 # Check if Module Exist
 Import-Module -Name PowervRA
-
 
 # setup package folder
 $path = "./package"
@@ -11,10 +7,3 @@ If(!(test-path $path))
 {
       New-Item -ItemType Directory -Force -Path $path
 }
-
-# Init credantials
-$secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
-
-#connect to the source vRA instance
-Connect-vRAServer -Server $sourceVra -Tenant $tenant -Credential $cred  -IgnoreCertRequirements
