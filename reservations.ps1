433692e0cbd529f70e6966bc458fd41172bbde2e
Edouard Topin <ed.topin@gmail.com>
2019-07-12 17:47:37 +0200

    change path

--- reservations.ps1
+++ reservations.ps1
@@ -12,5 +12,5 @@ $cred = New-Object System.Management.Automation.PSCredential ($username, $secpas
 #connect to the source vRA instance
 Connect-vRAServer -Server $sourceVra -Tenant $tenant -Credential $cred  -IgnoreCertRequirements
 
-Get-vRAReservation| Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File $path/reservations.json
+Get-vRAReservation| Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File package/reservations.json
 
