ed1e1cd2a7b4f1bcc7fa547f903cdd0e04744e08
Edouard Topin <ed.topin@gmail.com>
2019-07-12 18:42:13 +0200

    source/destination

---
+++ getBusinessGroups.ps1
@@ -0,0 +1,19 @@
+# Parameters
+param(
+    [string]$target
+)
+
+$filePath = "sessions/" + $target + ".json"
+
+# Get session information from json file
+$s = GET-Content $filePath | ConvertFrom-Json
+
+# Init credantials Obj
+$secpasswd = ConvertTo-SecureString $s.password -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($s.username, $secpasswd)
+
+
+# Connect to the source vRA instance
+Connect-vRAServer -Server $s.url -Tenant $s.tenant -Credential $cred  -IgnoreCertRequirements
+
+Get-vRABusinessGroup | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File "configurations/businessGroups.json"
