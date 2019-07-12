d24abc056e3fa0a4984bd72907b4426f1efc04a0
Edouard Topin <ed.topin@gmail.com>
2019-07-12 12:31:55 +0200

    hello World

---
+++ script.ps1
@@ -0,0 +1,22 @@
+$sourceVra = "cava-n-80-154.eng.vmware.com"
+$tenant = "vsphere.local"
+$username = "etopin@vsphere.local"
+$password = "VMware1!"
+
+# Init credantials
+$secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
+
+#connect to the source vRA instance
+Connect-vRAServer -Server $sourceVra -Tenant $tenant -Credential $cred  -IgnoreCertRequirements
+
+#create json files
+Get-vRAContent | Select-Object -Property * -ExcludeProperty Id, DateCreated, CreatedDate, LastUpdated | ConvertTo-Json | Out-File ./package/contents.json
+
+Get-vRABusinessGroup | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File ./package/businessGroups.json
+
+Get-vRABlueprint | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File ./package/blueprints.json
+
+Get-vRAEntitlement | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File ./package/entitlements.json
+
+Get-vRAReservation| Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File ./package/reservations.json
