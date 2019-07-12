40d6cbdc77a0b6cfc541e4bda39ad8c77e174d26
Edouard Topin <ed.topin@gmail.com>
2019-07-12 16:47:54 +0200

    test split

--- script.ps1
+++ script.ps1
@@ -1,24 +1,3 @@
-param(
-[string]$sourceVra,$tenant, $username, $password
-)
-# Check if Module Exist
-Import-Module -Name PowervRA
-
-
-# setup package folder
-$path = "./package"
-If(!(test-path $path))
-***REMOVED***
-      New-Item -ItemType Directory -Force -Path $path
-***REMOVED***
-
-# Init credantials
-$secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
-
-#connect to the source vRA instance
-Connect-vRAServer -Server $sourceVra -Tenant $tenant -Credential $cred  -IgnoreCertRequirements
-
 #create json files
 Get-vRAContent | Select-Object -Property * -ExcludeProperty Id, DateCreated, CreatedDate, LastUpdated | ConvertTo-Json | Out-File $path/contents.json
 
