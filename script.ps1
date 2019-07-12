c91f682e07c150be73e6f2a6b5b744fceac05370
Edouard Topin <ed.topin@gmail.com>
2019-07-12 17:23:59 +0200

    split files

--- script.ps1
+++ script.ps1
@@ -2,9 +2,9 @@
 Get-vRAContent | Select-Object -Property * -ExcludeProperty Id, DateCreated, CreatedDate, LastUpdated | ConvertTo-Json | Out-File $path/contents.json
 
 Get-vRABusinessGroup | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File $path/businessGroups.json
-
+#!/usr/bin/pwsh
 Get-vRABlueprint | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File $path/blueprints.json
-
+#!/usr/bin/pwsh
 Get-vRAEntitlement | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File $path/entitlements.json
-
+#!/usr/bin/pwsh
 Get-vRAReservation| Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File $path/reservations.json
