e173af4c3dc89cde98f0a352530102ea6b8a13d5
Edouard Topin <ed.topin@gmail.com>
2019-07-12 16:45:45 +0200

    folder creation

--- script.ps1
+++ script.ps1
@@ -1,8 +1,16 @@
 param(
 [string]$sourceVra,$tenant, $username, $password
 )
-
+# Check if Module Exist
 Import-Module -Name PowervRA
+
+
+# setup package folder
+$path = "./package"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
 
 # Init credantials
 $secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
@@ -12,12 +20,12 @@ $cred = New-Object System.Management.Automation.PSCredential ($username, $secpas
 Connect-vRAServer -Server $sourceVra -Tenant $tenant -Credential $cred  -IgnoreCertRequirements
 
 #create json files
-Get-vRAContent | Select-Object -Property * -ExcludeProperty Id, DateCreated, CreatedDate, LastUpdated | ConvertTo-Json | Out-File ./package/contents.json
+Get-vRAContent | Select-Object -Property * -ExcludeProperty Id, DateCreated, CreatedDate, LastUpdated | ConvertTo-Json | Out-File $path/contents.json
 
-Get-vRABusinessGroup | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File ./package/businessGroups.json
+Get-vRABusinessGroup | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File $path/businessGroups.json
 
-Get-vRABlueprint | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File ./package/blueprints.json
+Get-vRABlueprint | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File $path/blueprints.json
 
-Get-vRAEntitlement | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File ./package/entitlements.json
+Get-vRAEntitlement | Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File $path/entitlements.json
 
-Get-vRAReservation| Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File ./package/reservations.json
+Get-vRAReservation| Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File $path/reservations.json
