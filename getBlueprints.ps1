8fb5bdc3d1bf24e1b3b6f57bb9d1286e1e70e8b6
Edouard Topin <ed.topin@gmail.com>
2019-07-16 14:38:14 +0200

    print values for jenkins

--- getBlueprints.ps1
+++ getBlueprints.ps1
@@ -4,8 +4,13 @@ param(
 )
 
 # Connect to the source vRA instance
-# ./connectToServer.ps1 $target
 Get-Content ./cache_session.json | ConvertFrom-Json | Set-Variable vRAConnection
 
-# Parse values and save as JSON file
-Get-vRABlueprint | Select-Object -Property * -ExcludeProperty Id, LastUpdated, CreatedDate, Version | ConvertTo-Json| Out-File "configurations/blueprints.json"
+# Get  object and remove some Properties
+$object = Get-vRABlueprint | Select-Object -Property * -ExcludeProperty Id, LastUpdated, CreatedDate, Version
+
+# Print value
+Write-Output -InputObject $object
+
+# Convert to JSON file
+ConvertTo-Json -InputObject $object| Out-File "configurations/blueprints.json"
--- getContents.ps1
+++ getContents.ps1
@@ -4,8 +4,13 @@ param(
 )
 
 # Connect to the source vRA instance
-# ./connectToServer.ps1 $target
 Get-Content ./cache_session.json | ConvertFrom-Json | Set-Variable vRAConnection
 
-# Parse values and save as JSON file
-Get-vRAContent | Select-Object -Property * -ExcludeProperty Id, DateCreated, CreatedDate, LastUpdated | ConvertTo-Json | Out-File "configurations/contents.json"
+# Get  object and remove some Properties
+$object = Get-vRAContent | Select-Object -Property * -ExcludeProperty Id, DateCreated, CreatedDate, LastUpdated
+
+# Print value
+Write-Output -InputObject $object
+
+# Convert to JSON file
+ConvertTo-Json -InputObject $object| Out-File "configurations/contents.json"
