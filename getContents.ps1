3bcec363184c40a3e04d7c7d52cac3fbb71f21be
Edouard Topin <ed.topin@gmail.com>
2019-07-16 13:56:10 +0200

    split code

--- getContents.ps1
+++ getContents.ps1
@@ -3,17 +3,8 @@ param(
     [string]$target
 )
 
-$filePath = "sessions/" + $target + ".json"
-
-# Get session information from json file
-$s = GET-Content $filePath | ConvertFrom-Json
-
-# Init credantials Obj
-$secpasswd = ConvertTo-SecureString $s.password -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ($s.username, $secpasswd)
-
-
 # Connect to the source vRA instance
-Connect-vRAServer -Server $s.url -Tenant $s.tenant -Credential $cred  -IgnoreCertRequirements
+connectToServer.ps1 $target
 
+# Parse values and save as JSON file
 Get-vRAContent | Select-Object -Property * -ExcludeProperty Id, DateCreated, CreatedDate, LastUpdated | ConvertTo-Json | Out-File "configurations/contents.json"
