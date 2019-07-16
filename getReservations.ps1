acd768f8df26298be4e293404450601270799da6
Edouard Topin <ed.topin@gmail.com>
2019-07-16 16:56:32 +0200

    better filter & updated ref

--- getReservations.ps1
+++ getReservations.ps1
@@ -7,7 +7,7 @@ param(
 Get-Content ./cache_session.json | ConvertFrom-Json | Set-Variable vRAConnection
 
 # Get  object and remove some Properties
-$object = Get-vRAReservation | Sort-Object @{ e = 'Id'; a = $true } | Select-Object -Property * -ExcludeProperty Id
+$object = Get-vRAReservation | Sort-Object @{ e = 'Id'; a = $true } | Select-Object -Property * -ExcludeProperty Id, LastUpdated,CreatedDate,Version, SubTenantId -ExpandProperty ExtensionData
 
 # Print value
 Write-Output -InputObject $object
