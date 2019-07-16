cedb461a470e402b584c4852db50936d365887b4
Edouard Topin <ed.topin@gmail.com>
2019-07-16 11:28:38 +0200

    quick fix

--- getReservations.ps1
+++ getReservations.ps1
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-$s = GET-Content ./session.json | ConvertFrom-Json
- $sourceVra = $s.url
- $tenant = $s.tenant
- $username = $s.username
- $password = $s.password
-
-# Init credantials
-$secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
-
-
-#connect to the source vRA instance
-Connect-vRAServer -Server $sourceVra -Tenant $tenant -Credential $cred  -IgnoreCertRequirements
-
-Get-vRAReservation| Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File package/reservations.json
-=======
 # Parameters
 param(
     [string]$target
@@ -34,5 +17,4 @@ $cred = New-Object System.Management.Automation.PSCredential ($s.username, $secp
 Connect-vRAServer -Server $s.url -Tenant $s.tenant -Credential $cred  -IgnoreCertRequirements
 
 Get-vRAReservation| Select-Object -Property * -ExcludeProperty Id | ConvertTo-Json | Out-File configurations/reservations.json
->>>>>>> ad25df7526708199e7c783e943cdc7d981e4b737
 
